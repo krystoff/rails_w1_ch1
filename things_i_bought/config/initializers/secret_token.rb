d27e00ed589a020107e301303a1bb2fa0231964a
Chris Larson <krystoff@uw.edu>
2012-01-22 15:07:57 -0800

    Rails week1 Ch1 assignment

---
+++ things_i_bought/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThingsIBought::Application.config.secret_token = '651ae194b472935f2ea1644050bdde30de7b27928508bde4b20ae02a2019bd5fa0072871a64a1f81914434ec353569cce3f940bbd4196495ca6975d1c38a3f8a'
